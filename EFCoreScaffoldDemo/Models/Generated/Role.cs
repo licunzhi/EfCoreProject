177104f03ce71a9ed6edfd2a54824864f478e803
licunzhi <licunzhi2015@outlook.com>
2023-05-06 20:27:46 +0800

    commit demo code

---
+++ EFCoreScaffoldDemo/Models/Generated/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreScaffoldDemo.Models
+{
+    [Index("UserId", Name = "IX_Roles_UserId")]
+    public partial class Role
+    {
+        [Key]
+        public long Id { get; set; }
+        public string RoleName { get; set; } = null!;
+        public long UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        [InverseProperty("Roles")]
+        public virtual User User { get; set; } = null!;
+    }
+}
---
+++ EFCoreScaffoldDemo/Models/Generated/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreScaffoldDemo.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Roles = new HashSet<Role>();
+        }
+
+        [Key]
+        public long Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+
+        [InverseProperty("User")]
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
